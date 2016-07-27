0bc394436e29c0bd4500ca80e03a3447b70b3f0a
justaname01 <charles.rittenhouse6@gmail.com>
2016-07-27 14:52:53 -0500

    Update game.cpp
    Added more code to round out the game() function. Am committing now to mark my place.

--- game.cpp
+++ game.cpp
@@ -54,5 +54,53 @@ char menu()
 
 void game()
 {
-	std::cout << "\n";
+	Deck deck;		//the main deck
+	Deck dealer;		//for keeping track of the dealer's hand
+	Deck player;		//for keeping track of the player's hand
+	
+	player.clearDeck();	//clear the player's hand
+	dealer.clearDeck();	//clear the dealer's hand
+	int gameVal = 0;
+	do 
+	{
+		std::cout << "\n\n\n";
+		if(gameVal == 21)
+		{
+			std::cout << "Winner!\n";
+			break;
+		}
+		
+		std::cout << "You have:\n";
+		for(int index = 0; index < player.getDeck().size(); index++)
+		{
+			
+		}
+	}while(gameVal <= 21);
+}
+
+char deal()
+{
+	bool isValid = false;
+	char choice(0);
+
+	do
+	{
+		std::cout << "(p)lay\n";
+		std::cout << "(q)uit\n" << std::endl;
+		std::cout << "==> ";
+
+		std::cin >> choice;
+		
+		switch(choice)
+		{
+			case 'p':
+			case 'q':
+				isValid = true;
+				break;
+			default:
+				break;
+		}
+	}while( !isValid );
+
+	return choice;
 }
